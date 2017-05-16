3765a77ee60553ae087e2302395adf93a5710f68
jonross <jonross88@gmail.com>
2017-05-16 18:07:58 -0400

    initial project setup

---
+++ SpikeSwarm/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// TODO: setup pooled updates
+		if (!isLocalPlayer)
+			return;
+
+		var x = Input.GetAxis("Horizontal") * 0.1f;
+		var z = Input.GetAxis("Vertical") * 0.1f;
+
+		transform.Translate(x, 0, z);
+	}
+
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent<MeshRenderer>().material.color = Color.red;
+	}
+}
---
+++ SpikeSwarm/Assets/Scripts/StateMachine.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine
+{
+	private List<State> _states;
+	private State _currentState = null;
+
+	public class State
+	{
+		public virtual void Enter() { }
+		public virtual void Update() { }
+		public virtual void Exit() { }
+	}
+
+	public void AddState(State state)
+	{
+		if (state == null)
+			return;
+
+		_states.Add(state);
+	}
+
+	public void ChangeState<T>() where T : State, new()
+	{
+		State state = _states.Find(x => x.GetType() == typeof(T));
+
+		// State does not exist yet, add it
+		if(state == null)
+		{
+			state = new T();
+			_states.Add(state);
+		}
+
+		if(_currentState != null)
+		{
+			_currentState.Exit();
+		}
+
+		_currentState = state;
+		_currentState.Enter();
+	}
+
+	public void ChangeState(State nextState)
+	{
+
+	}
+}
